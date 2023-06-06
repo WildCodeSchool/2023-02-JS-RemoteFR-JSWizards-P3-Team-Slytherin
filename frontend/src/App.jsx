05681256ec8fb58e4283c788737f23f6aeba1b10
Vinch <vince.daviaud@gmail.com>
2023-06-06 16:53:55 +0200

    add logo + Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
+import Header from "./components/Header";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="bg-winebg bg-cover bg-center">
-      <div className="bg-black w-screen h-screen opacity-70">
-        <Home />
+    <>
+      <Header />
+      <div className="bg-winebg bg-cover bg-center">
+        <div className="bg-[black] h-screen opacity-70">
+          <Home />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="bg-primary h-24 flex items-center justify-between">
+      <img src="/assets/logo/logo_white.png" className="h-16" alt="" />
+      <nav className="">bato</nav>
+    </header>
+  );
+}
+
+export default Header;
