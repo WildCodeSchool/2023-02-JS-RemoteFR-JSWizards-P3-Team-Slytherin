0e3d3f6bbcce83bb9d00f156ea27f1d22d391f20
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-06-07 15:13:28 +0200

    create catalogue page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Page404 from "./pages/Page404";
 import Home from "./pages/Home";
+import Catalogue from "./pages/Catalogue";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Page404 />} />
+      <Route path="catalogue" element={<Catalogue />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/Catalogue.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Catalogue = () => {
+  return (
+    <Layout>
+      <h2>Hello</h2>
+    </Layout>
+  );
+};
+
+export default Catalogue;
