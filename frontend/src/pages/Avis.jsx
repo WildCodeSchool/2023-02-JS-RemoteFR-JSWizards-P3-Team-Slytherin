a022f9aaa848ceb5c69560ab41bc490bb1083fc2
Samuel Faber <samuel.faber@live.fr>
2023-06-08 17:08:33 +0200

    Gestion du responsive

--- frontend/src/pages/Avis.jsx
+++ frontend/src/pages/Avis.jsx
@@ -41,12 +41,12 @@ const note = [
 export default function Avis() {
   return (
     <Layout>
-      <div className="w-full flex flex-col items-center justify-center gap-4 pt-4">
+      <div className="w-full h-full flex flex-col items-center justify-center gap-4 pt-4">
         <h2 className="text-3xl">Votre avis nous intéresse</h2>
-        <form className="w-full flex flex-col gap-4">
+        <form className="w-full flex flex-col gap-4 pt-4">
           <ul className="flex flex-col gap-4">
-            <li className="flex justify-between items-center">
-              <p className="text-large">
+            <li className="flex justify-between items-center flex-nowrap">
+              <p className="text-sm max-w-[50%] md:max-w-[480px] md:text-lg">
                 Comment avez-vous trouvé le choix des différents cépages ?
               </p>
               <div className="flex">
@@ -60,8 +60,8 @@ export default function Avis() {
                 ))}
               </div>
             </li>
-            <li className="flex justify-between">
-              <p className="text-large">
+            <li className="flex justify-between items-center flex-nowrap">
+              <p className="text-sm max-w-[50%] md:max-w-[480px] md:text-lg">
                 Comment avez-vous trouvé l'ergonomie de l'application ?
               </p>
               <div className="flex">
@@ -75,8 +75,8 @@ export default function Avis() {
                 ))}
               </div>
             </li>
-            <li className="flex justify-between">
-              <p className="text-large">
+            <li className="flex justify-between items-center flex-nowrap">
+              <p className="text-sm max-w-[50%] md:max-w-[480px] md:text-lg">
                 Comment avez-vous trouvé l'animation ?
               </p>
               <div className="flex">
@@ -91,11 +91,22 @@ export default function Avis() {
               </div>
             </li>
           </ul>
-          <label className="text-large" htmlFor="commentaire">
+          <label className="text-sm md:text-lg pt-4" htmlFor="commentaire">
             Avez-vous un commentaire ou des suggestions à nous laisser ?
           </label>
-          <textarea name="commentaire" id="commentaire" cols="30" rows="10" />
-          <button type="submit">Envoyer votre avis</button>
+          <textarea
+            className="p-2"
+            name="commentaire"
+            id="commentaire"
+            placeholder="J'ai trouvé cet atelier génial, par contre si j'avais une suggestion ce serait de..."
+            cols="30"
+            rows="5"
+          />
+          <div className="w-full flex justify-center pt-4">
+            <button className="p-2" type="button">
+              Envoyer votre avis
+            </button>
+          </div>
         </form>
       </div>
     </Layout>
