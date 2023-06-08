65f324ddc5852c15b839df197b9cd7e686a5e6a4
Samuel Faber <samuel.faber@live.fr>
2023-06-08 12:20:36 +0200

    [Fix] Correction mineure CSS

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -7,8 +7,8 @@ export default function Layout({ children }) {
       <Header />
       <div className="h-screen bg-winebg bg-cover bg-center">
         <div className="h-screen bg-[black] bg-opacity-70 flex justify-center">
-          <div className="text-secondary overflow-y-auto w-[80%] pt-24">
-            <main className="h-full">{children}</main>
+          <div className="text-secondary w-[80%] pt-24">
+            <main className="h-full overflow-y-auto">{children}</main>
           </div>
         </div>
       </div>
