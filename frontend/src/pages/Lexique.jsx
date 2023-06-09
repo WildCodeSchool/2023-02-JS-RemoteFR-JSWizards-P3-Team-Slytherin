e672e030b31ad7345ac661343da59e89c3e8f878
Samuel Faber <samuel.faber@live.fr>
2023-06-09 15:03:46 +0200

    [Fix] Suppression du bouton 'X' qui fait double emploi

--- frontend/src/pages/Lexique.jsx
+++ frontend/src/pages/Lexique.jsx
@@ -51,34 +51,11 @@ function Lexique() {
       setLexiqueDBFilter(lexiqueDB);
     }
   };
-  const handleClick = () => {
-    setSearch("");
-    setLexiqueDBFilter(lexiqueDB);
-  };
+
   return (
     <Layout>
       <div className="text-secondary pt-4">
-        <div className="flex flex-col gap-4 items-center md:flex-row md:justify-between px-4">
-          <p className="italic px-0 text-xs md:text-sm md:px-0">
-            Source :{" "}
-            <a
-              href="http://www.vin-vigne.com"
-              title="Vin Vigne: Guide des vins et des vignes de France"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Vin-Vigne.com
-            </a>
-            {", "}
-            <a
-              href="http://www.vin-vigne.com/cepage/"
-              title="Liste des cépages"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Liste des cépages
-            </a>
-          </p>
+        <div className="flex gap-4 items-center flex-row justify-around px-4">
           <button type="button" onClick={goBack} className="text-center">
             Retour
           </button>
@@ -93,13 +70,6 @@ function Lexique() {
                 onChange={handleChange}
               />
             </form>
-            <button
-              className="p-1 pb-2 flex justify-center text-xl font-bold items-center bg-secondary rounded-full w-6 h-6 text-primary"
-              type="button"
-              onClick={handleClick}
-            >
-              &times;
-            </button>
           </div>
         </div>
         <div className="flex flex-col 10/12 mx-12 xl:mx-20">
@@ -118,65 +88,6 @@ function Lexique() {
               >
                 [en savoir plus...]
               </a>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
-              accusamus consectetur aperiam aut exercitationem magnam voluptates
-              obcaecati facere similique aliquam laudantium fugiat iure
-              repellendus rerum consequuntur nobis, at eveniet iste omnis
-              adipisci officia modi nesciunt! Dolorum unde ad aperiam, molestias
-              neque cum officia non at iste a exercitationem iure pariatur
-              nostrum dicta quam facilis perspiciatis itaque nihil quod quia
-              nisi qui reprehenderit deserunt. Assumenda, quae? Aspernatur ea
-              assumenda soluta rem voluptatem error, quia rerum placeat, eum id
-              reprehenderit iste, voluptas nihil ad perferendis totam? Voluptas
-              nemo est tenetur voluptatum eos fugiat neque consequuntur, iste
-              repudiandae debitis in quia temporibus id obcaecati tempore iure
-              eaque, quasi iusto, nam rerum error totam. Quod aliquam, nisi
-              cupiditate quasi quaerat, nostrum quis ipsum nam quibusdam
-              voluptatibus ab architecto vel adipisci repellat inventore!
-              Laborum exercitationem cumque nam quisquam necessitatibus
-              laboriosam ducimus. Quidem, veniam asperiores iusto consequuntur,
-              nisi ea, enim quam vitae eligendi inventore odit maiores? Quo,
-              tempore. Cum adipisci ut consequatur corrupti modi veniam harum
-              dolores, placeat pariatur neque quae at repellendus veritatis
-              quidem illo facere ab magni nihil reiciendis inventore a
-              voluptatibus minus illum labore? Nesciunt qui corporis deserunt
-              omnis consequatur reiciendis, facilis aspernatur optio nam eaque
-              debitis distinctio ea sapiente harum cum eius ratione amet
-              voluptates, et ipsa dolorem esse repudiandae. Enim soluta eum
-              alias sequi ullam quam tempore officia vitae. Assumenda ut
-              accusamus animi, corrupti pariatur ipsa ipsam non blanditiis,
-              voluptatum tempora cupiditate voluptas aut adipisci soluta vero!
-              Quas, sequi? Corrupti, autem animi quisquam quis impedit in
-              veritatis at, beatae excepturi quod sunt vero? Ipsum aspernatur
-              dicta, tempore iure deleniti magni quo dignissimos praesentium
-              molestias aperiam. Ad consequatur molestiae, minima pariatur quas
-              nam doloremque consectetur soluta nihil! Rerum ipsam id, suscipit
-              tempore tempora non a atque, hic esse blanditiis neque cupiditate
-              quae quod qui nam officiis excepturi! Tenetur atque beatae
-              accusamus deleniti eligendi esse culpa possimus excepturi illum
-              non magni veritatis voluptatum, saepe sed quos eveniet, rem fugit
-              corporis soluta aspernatur animi. Vero ipsum quasi, laudantium
-              voluptatum commodi quibusdam accusamus minus maiores? Est natus
-              voluptas aliquid at fugiat repudiandae porro dolores tenetur ut
-              nisi commodi illo, itaque, aliquam, eligendi magnam nesciunt rerum
-              enim possimus numquam? Id ipsam repellendus sint laborum possimus
-              repudiandae soluta voluptas blanditiis tempore pariatur aliquid
-              ullam quibusdam nisi officia dicta neque at, ea necessitatibus
-              velit quod magnam amet temporibus consequatur quasi. Suscipit
-              asperiores veritatis impedit animi incidunt et alias molestias
-              dolore assumenda architecto fugiat dolor cum aspernatur odio
-              error, cupiditate nobis qui consectetur consequuntur sapiente illo
-              nihil iure quam deserunt. Nesciunt quae ut vel facere repellat
-              sunt quod praesentium deleniti ad laudantium sapiente nobis
-              mollitia cum atque nostrum neque nulla dolor itaque aliquid
-              quibusdam eveniet perferendis nam, id a? Ipsam quaerat sed alias
-              repellendus, repudiandae saepe, aliquid id, vel fugiat vitae quo
-              eaque harum dicta reiciendis? Nam, eligendi eius necessitatibus ex
-              perferendis vitae debitis molestiae optio, autem neque deserunt
-              voluptatibus dolorem minus nisi eos quod! Explicabo natus, illum
-              quaerat impedit in velit aperiam, earum voluptatum dolore
-              architecto nam quae exercitationem unde provident esse assumenda
-              vitae excepturi reprehenderit. Sequi, harum.
             </p>
           ))}
         </div>
