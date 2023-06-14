aa511913149451b318eace21d185fcef8c8e44fa
Samuel Faber <samuel.faber@live.fr>
2023-06-14 14:25:55 +0200

    Création de la base des fichiers

---
+++ frontend/src/pages/FicheDegustation/GustatifPartOne.jsx
@@ -0,0 +1,9 @@
+import Layout from "@components/Layout";
+
+export default function GustatifPartOne() {
+  return (
+    <Layout>
+      <div>GustatifPartOne</div>
+    </Layout>
+  );
+}
---
+++ frontend/src/pages/FicheDegustation/Olfactif.jsx
@@ -0,0 +1,9 @@
+import Layout from "@components/Layout";
+
+export default function Olfactif() {
+  return (
+    <Layout>
+      <div>Olfactif</div>
+    </Layout>
+  );
+}
