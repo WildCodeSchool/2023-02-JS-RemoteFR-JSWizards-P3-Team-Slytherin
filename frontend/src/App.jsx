6cd6048da98bbd5edfd75e51cdeffb49a3b21b92
Samuel Faber <samuel.faber@live.fr>
2023-06-14 14:36:12 +0200

    Création des routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,13 @@ import Profil from "@pages/Profil";
 import Lexique from "@pages/Lexique";
 import Avis from "@pages/Avis";
 import ProfilDegustation from "@pages/ProfilDegustation";
+import Visuel from "@pages/FicheDegustation/Visuel";
+import Olfactif from "@pages/FicheDegustation/Olfactif";
+import GustatifPartOne from "@pages/FicheDegustation/GustatifPartOne";
+import GustatifPartTwo from "@pages/FicheDegustation/GustatifPartTwo";
+import Final from "@pages/FicheDegustation/Final";
 import Page404 from "@pages/Page404";
+
 import "./App.css";
 
 function App() {
@@ -16,6 +22,11 @@ function App() {
       <Route path="/avis" element={<Avis />} />
       <Route path="/lexique" element={<Lexique />} />
       <Route path="/selection" element={<NotreSelection />} />
+      <Route path="/fiche/visuel" element={<Visuel />} />
+      <Route path="/fiche/olfactif" element={<Olfactif />} />
+      <Route path="/fiche/gustatif-part1" element={<GustatifPartOne />} />
+      <Route path="/fiche/gustatif-part2" element={<GustatifPartTwo />} />
+      <Route path="/fiche/final" element={<Final />} />
       <Route path="/profil" element={<Profil />} />
       <Route
         path="/profil/profil_degustation"
