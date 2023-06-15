fd631ca71b654e2e36c82637a641d0dad37fcfb8
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-06-15 17:39:18 +0200

    add confirmation password

--- frontend/src/pages/NotreSelection.jsx
+++ frontend/src/pages/NotreSelection.jsx
@@ -1,11 +1,37 @@
-import Selection from "../components/Selection";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 import data from "../components/Data/data-wine";
 
 export default function NotreSelection() {
   return (
     <Layout>
-      <Selection wines={data} />
+      <h2 className="text-center font-bold text-2xl p-12 text-secondary">
+        Notre sélection de vins
+      </h2>
+
+      <div className="flex sm:mx-10 gap-14 flex-wrap justify-center md:mx-24 md:gap-24 lg:mx-42 lg:gap-20">
+        {data.map((wine) => (
+          <div key={wine.id}>
+            <img
+              className="object-cover w-[160px] h-[187px] rounded-t-xl"
+              src={wine.image}
+              alt={wine.name}
+            />
+
+            <h3 className="bg-secondary rounded-b-xl text-primary text-center p-2">
+              {wine.name}
+            </h3>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex justify-center">
+        <Link to="/profil/profil_degustation">
+          <button type="button" className="m-12">
+            Profil dégustation
+          </button>
+        </Link>
+      </div>
     </Layout>
   );
 }
--- frontend/src/pages/ProfilModif.jsx
+++ frontend/src/pages/ProfilModif.jsx
@@ -1,16 +1,86 @@
-import ModifProfil from "@components/ModifProfil";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 import clients from "../components/Data/data-client";
 
 function ProfilModif() {
   return (
     <Layout>
-      <ModifProfil
-        name={clients[0].name}
-        lastname={clients[0].lastname}
-        email={clients[0].email}
-        birthday={clients[0].birthday}
-      />
+      <div className="flex flex-col items-center p-10">
+        <h2 className="text-xl">Modifier mon profil</h2>
+        <span className="w-56 bg-secondary h-[1px] self-center m-6" />
+
+        <form className=" flex flex-col gap-6">
+          <div className="flex flex-row justify-between gap-5">
+            <label htmlFor="name">Prénom:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder={clients[0].name}
+              className="text-primary w-44 p-1 rounded text-center"
+            />
+          </div>
+          <div className="flex justify-between flex-row gap-5">
+            <label htmlFor="lastname">Nom:</label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              placeholder={clients[0].lastname}
+              className="text-primary w-44 p-1 rounded text-center"
+            />
+          </div>
+          <div className="flex justify-between flex-row gap-5">
+            <label htmlFor="email">Adresse mail:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder={clients[0].email}
+              className="text-primary w-44 p-1 rounded text-center"
+            />
+          </div>
+          <div className="flex justify-between flex-row gap-5">
+            <label htmlFor="birthday">Date de naissance:</label>
+            <input
+              type="text"
+              name="birthday"
+              id="birthday"
+              placeholder={clients[0].birthday}
+              className="text-primary w-44 p-1 rounded text-center"
+            />
+          </div>
+          <div className="flex justify-between flex-row gap-5">
+            <label htmlFor="password">Mot de passe:</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              className="text-primary w-44 p-1 rounded text-center"
+            />
+          </div>
+          <div className="flex justify-between flex-row gap-5">
+            <label htmlFor="password">Confirmer mot de passe:</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              className="text-primary w-44 p-1 rounded text-center"
+            />
+          </div>
+        </form>
+        <div className="p-8 flex flex-row justify-center flex-wrap">
+          <Link to="/profil">
+            <button className="m-2" type="button">
+              Valider
+            </button>
+
+            <button className="m-2" type="button">
+              Annuler
+            </button>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
