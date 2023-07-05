3570853f371716b307eeaaa26d5c7da8691d79ec
Youcef Aissat <y.aissat@gmail.com>
2023-07-05 13:32:25 +0200

    ajout page admin_utilisateur

---
+++ frontend/src/components/Data/data-utilisateur-admin.jsx
@@ -0,0 +1,64 @@
+const data = [
+  {
+    id: 1,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 2,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 3,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 4,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 5,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 6,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 7,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 8,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 9,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+  {
+    id: 10,
+    lastName: "Doe",
+    firstName: "John",
+    email: "john.d@google.com",
+  },
+];
+
+export default data;
--- frontend/src/pages/Admin/Utilisateurs.jsx
+++ frontend/src/pages/Admin/Utilisateurs.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import data from "../../components/Data/data-utilisateur-admin";
+
 export default function Utilisateurs() {
-  return <h1>Utilisateurs</h1>;
+  const [users, setUsers] = useState(data);
+
+  const handleDelete = (id) => {
+    const newUsers = users.filter((user) => user.id !== id);
+    setUsers(newUsers);
+  };
+
+  return (
+    <div className="flex flex-col justify-center mt-8">
+      <h1 className="flex justify-center mb-8 text-2xl font-bold">
+        Utilisateurs
+      </h1>
+      <table className="border-collapse border border-black">
+        <tr className="">
+          <th className="border border-black">N°</th>
+          <th className="border border-black">Nom</th>
+          <th className="border border-black">Prénom</th>
+          <th className="border border-black">Email</th>
+          <th className="border border-black">Supprimer</th>
+        </tr>
+        {users.map((user) => (
+          <tr key={user.id} className="text-center">
+            <td className="border border-black">{user.id}</td>
+            <td className="border border-black">{user.lastName}</td>
+            <td className="border border-black">{user.firstName}</td>
+            <td className="border border-black">{user.email}</td>
+            <td className="border border-black">
+              <button
+                type="button"
+                onClick={() => handleDelete(user.id)}
+                className="mx-auto transparent-button"
+              >
+                <img
+                  src="../../../public/assets/delete/delete.png"
+                  alt="supprimer utilisateur"
+                  className="w-4"
+                />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
