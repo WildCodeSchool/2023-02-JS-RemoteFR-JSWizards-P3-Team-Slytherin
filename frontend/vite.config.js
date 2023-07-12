341c5afea6f4fc32da42b23aea1857ed953b28c5
Samuel Faber <samuel.faber@live.fr>
2023-07-12 10:15:10 +0200

    Ajout d'un alias @contexts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
       "@services": path.resolve(__dirname, "src/services"),
     },
   },
