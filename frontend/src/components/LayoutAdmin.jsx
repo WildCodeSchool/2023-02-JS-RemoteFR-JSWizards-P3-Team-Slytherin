7f057a384df335ee92f4c99271988ecd060c70af
Vinch <vince.daviaud@gmail.com>
2023-07-24 11:40:09 +0200

    style admin scrollbar

--- frontend/src/components/LayoutAdmin.jsx
+++ frontend/src/components/LayoutAdmin.jsx
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <>
       <HeaderAdmin />
-      <div className="h-screen bg-winebg bg-cover bg-center">
+      <div className="h-screen bg-winebg bg-cover bg-center admin-scroll-bar">
         <div className="h-screen bg-secondary bg-opacity-70 flex justify-center">
           <div className="text-[black] w-[80%] pt-24">
             <main className="h-full overflow-y-auto">{children}</main>
